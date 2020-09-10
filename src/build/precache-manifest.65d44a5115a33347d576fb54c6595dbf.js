4b91bd790836176777c46e37e5a00335a5157a11
dacalico <perla50@gmail.com>
2020-09-09 22:29:51 -0400

    first commit

---
+++ src/build/precache-manifest.65d44a5115a33347d576fb54c6595dbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70484341c4ccd67c6083e0161080d9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6463b03ae81438a7041",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6463b03ae81438a7041",
+    "url": "/static/js/main.bf681f39.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
